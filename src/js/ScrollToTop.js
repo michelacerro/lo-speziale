190a16c2716d58f7db7e1c7cfe4dd9e6e01f30c6
Michela Cerro <78146321+michelacerro@users.noreply.github.com>
2021-12-13 21:28:20 +0100

    delete a console.log

--- src/js/ScrollToTop.js
+++ src/js/ScrollToTop.js
@@ -4,7 +4,6 @@ import {useLocation} from 'react-router-dom';
 
 const ScrollToTop = () => {
     const {pathname} = useLocation();
-    console.log('p', pathname);
 
     useEffect(() => {
         window.scrollTo(0, 0);
