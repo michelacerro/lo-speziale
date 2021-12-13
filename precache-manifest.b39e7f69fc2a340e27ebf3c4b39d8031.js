cb0f7c4cb7d291edf18f617f74150a06da555f06
Michela Cerro <78146321+michelacerro@users.noreply.github.com>
2021-12-13 21:46:00 +0100

    Updates

---
+++ precache-manifest.b39e7f69fc2a340e27ebf3c4b39d8031.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7c74aaf1354355a87a78aa294b9d5b",
+    "url": "/lo-speziale/index.html"
+  },
+  {
+    "revision": "7a25343e56dc15d0776f",
+    "url": "/lo-speziale/static/css/main.cc31f90f.chunk.css"
+  },
+  {
+    "revision": "03f4a0d918e1ce778d3d",
+    "url": "/lo-speziale/static/js/2.b91c3fce.chunk.js"
+  },
+  {
+    "revision": "8d0df6a9f6b2d88ac068d142fd4b4030",
+    "url": "/lo-speziale/static/js/2.b91c3fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e316e2860c0249670777",
+    "url": "/lo-speziale/static/js/3.e4aa2faa.chunk.js"
+  },
+  {
+    "revision": "7a25343e56dc15d0776f",
+    "url": "/lo-speziale/static/js/main.632da89a.chunk.js"
+  },
+  {
+    "revision": "4e033f6f624a99e7fd7f",
+    "url": "/lo-speziale/static/js/runtime-main.3763a550.js"
+  },
+  {
+    "revision": "edc4c3323812cce8f118a2c63e85e0cd",
+    "url": "/lo-speziale/static/media/background-image-vertical.edc4c332.jpg"
+  },
+  {
+    "revision": "c9753c8858dfb42f9f41277c4e8dd2ef",
+    "url": "/lo-speziale/static/media/background-image.c9753c88.jpg"
+  },
+  {
+    "revision": "78c4f7fc17d421d8635058dedff3f865",
+    "url": "/lo-speziale/static/media/school-1.78c4f7fc.jpg"
+  },
+  {
+    "revision": "1495cf0b8dab2bd70ee1fbdf5fa7bf8b",
+    "url": "/lo-speziale/static/media/school-2.1495cf0b.jpg"
+  },
+  {
+    "revision": "139e4ba1feded9d4227e86f7af815142",
+    "url": "/lo-speziale/static/media/school-3.139e4ba1.jpg"
+  },
+  {
+    "revision": "af020fbafe20e77bb79cd86e0bdc14b6",
+    "url": "/lo-speziale/static/media/school-4.af020fba.jpg"
+  },
+  {
+    "revision": "a3c1446b21467d657bf823bc8c722dd4",
+    "url": "/lo-speziale/static/media/school-5.a3c1446b.jpg"
+  },
+  {
+    "revision": "a61808e7c2e0c3d02e90ddf32a952f38",
+    "url": "/lo-speziale/static/media/school-6.a61808e7.jpg"
+  },
+  {
+    "revision": "e9243ea3f32d4dc9bc49b48787e632ca",
+    "url": "/lo-speziale/static/media/school-7.e9243ea3.jpg"
+  },
+  {
+    "revision": "3e426c8e18c8dd59ebb914923c1ffab9",
+    "url": "/lo-speziale/static/media/school-8.3e426c8e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lo-speziale/precache-manifest.b39e7f69fc2a340e27ebf3c4b39d8031.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lo-speziale/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
